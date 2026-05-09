4680d283cb80db0fefacf7dd3a4cf4fbf543b413
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-09 14:46:33 +0530

    debug the erros

--- core/auth.py
+++ core/auth.py
@@ -3,7 +3,7 @@ from storage.loader import load_users
 def login(username, password):
     data = load_users()
 
-    for user in data["user"]:
+    for user in data["users"]:
         if user["username"] == username and user["password"] == password:
             return user 
         
