e41c520fb7fa06777869754452083b7b7b4ff46f
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-09 14:54:09 +0530

    Integrate validation checks into main login flow

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from core.auth import login
 from core.session import set_current_user
 from core.permissions import has_permission
+from utils.validators import validate_username, validate_password
+
 
 username = input("Enter Username: ")
 password = input("Enter Password: ")
 
-user = login(username, password)
+
+if not validate_username(username):
+    print("Invalid username.")
+    exit()
+
+if not validate_password(password):
+    print("Invalid password.")
+    exit()
+
+
+    user = login(username, password)
+
 
 if user:
     set_current_user(user)
-
     print(f"Welcome {user['username']}")
 
     if has_permission(user, "admin"):
